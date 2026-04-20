c709381e4abc0c3fee60123dd353efc02a7a4f95
Glória Maria <alvugy@gmail.com>
2026-04-20 12:32:54 -0300

    initial commit (#1)
    * initial commit
    
    * chore: install all dependencies and scaffold folder structure

---
+++ src/main.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+
+import Home from './pages/Home'
+
+function App() {
+  return (
+    <Router basename="/react-example">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </ Router>
+  )
+}
+
+export default App
