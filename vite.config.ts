cb9f9d42a9c650a6c6c9af91c20869cb2babe477
Glória Maria <alvugy@gmail.com>
2026-04-20 14:05:16 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const basePath = mode === 'development'
-    ? process.env.VITE_BASE_PATH_DEV
-    : process.env.VITE_BASE_PATH_PROD
-
-  return {
-    base: basePath || '/',
-    plugins: [
-      react(),
-      tailwindcss(),
-    ],
-    build: {
-      outDir: 'dist',
-    },
-    define: {
-      '__APP_BASE_PATH__': JSON.stringify(basePath || '/')
-    }
-  }
+export default defineConfig({
+  base: '/pucpr-devops-aquarium',
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
